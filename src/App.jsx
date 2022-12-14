d18956baba33bf3f9e1c52c0d2755cf9050f1e54
Jordan Butterfield <jmcbutter@gmail.com>
2022-12-14 15:59:48 -0600

    Add Filters, add Drag and Drop functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,309 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.scss";
 import { SunIcon, MoonIcon, CrossIcon, CheckIcon } from "./assets/images/icons";
 import DesktopImg from "/src/assets/images/bg-desktop-dark.jpg";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [count, setCount] = useState(0);
   const [theme, setTheme] = useState("light");
+  const [tasks, setTasks] = useState([
+    { id: 0, name: "Task #1", completed: false },
+    { id: 1, name: "Task #2", completed: false },
+    { id: 2, name: "Task #3", completed: false },
+    { id: 3, name: "Task #4", completed: false },
+  ]);
+  const [filter, setFilter] = useState("All");
+  const [displayedTasks, setDisplayedTasks] = useState(tasks);
+  const [dragging, setDragging] = useState();
+
+  useEffect(() => {
+    let root = document.documentElement;
+    root.classList.remove("theme--light", "theme--dark");
+    root.classList.add(`theme--${theme}`);
+  }, [theme]);
+
+  function toggleTaskCompleted(id) {
+    const editedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return task.completed
+          ? { ...task, completed: false }
+          : { ...task, completed: true };
+      }
+      return task;
+    });
+
+    setTasks(editedTasks);
+  }
+
+  function deleteTask(id) {
+    const editedTasks = tasks.filter((task) => task.id != id);
+    setTasks(editedTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+
+    setTasks(editedTasks);
+  }
+
+  function filterTasks(filter) {
+    const filteredTasks = tasks.filter((task) => {
+      if (filter == "All") return true;
+      if (filter == "Active" && !task.completed) return true;
+      if (filter == "Completed" && task.completed) return true;
+      return false;
+    });
+
+    setDisplayedTasks(filteredTasks);
+  }
+
+  useEffect(() => filterTasks(filter), [filter, tasks]);
+
+  function clearCompleted() {
+    const editedTasks = tasks.filter((task) => !task.completed);
+    setTasks(editedTasks);
+  }
+
+  function reorderTask(positionRelativeToSibling, siblingId) {
+    const taskIndex = tasks.findIndex((task) => task.id == dragging);
+    const taskToInsert = tasks[taskIndex];
+    const editedTasks = tasks.filter((task) => task.id != dragging);
+    const siblingIndex = editedTasks.findIndex((task) => task.id == siblingId);
+
+    if (positionRelativeToSibling == "above") {
+      editedTasks.splice(siblingIndex, 0, taskToInsert);
+    } else {
+      editedTasks.splice(siblingId + 1, 0, taskToInsert);
+    }
+
+    setTasks(editedTasks);
+  }
+
+  function dragTask(id) {
+    setDragging(id);
+  }
 
   return (
-    <div className={`App theme--${theme}`}>
-      <div className="Content">
-        <div className="Heading">
+    <div className={`App`}>
+      <div className="content">
+        <div className="heading">
           <h1>TODO</h1>
-          {theme == "light" ? (
+          {theme == "dark" ? (
             <SunIcon
-              className="Heading__theme-switch"
-              onClick={() => setTheme("dark")}
+              className="heading__theme-switch"
+              onClick={() => setTheme("light")}
             />
           ) : (
             <MoonIcon
-              className="Heading__theme-switch"
-              onClick={() => setTheme("light")}
+              className="heading__theme-switch"
+              onClick={() => setTheme("dark")}
             />
           )}
         </div>
-        <Task />
+
+        <div className="task-input">
+          <TaskInput tasks={tasks} setTasks={setTasks} />
+        </div>
+
+        <div className="tasks">
+          {displayedTasks.map((task) => (
+            <Task
+              id={task.id}
+              name={task.name}
+              completed={task.completed}
+              key={task.id}
+              toggleTaskCompleted={toggleTaskCompleted}
+              deleteTask={deleteTask}
+              editTask={editTask}
+              draggable="true"
+              reorderTask={reorderTask}
+              dragTask={dragTask}
+            />
+          ))}
+
+          <div className="task-info card">
+            <div className="task-info__remaining">
+              {tasks.filter((task) => !task.completed).length} Tasks Remaining
+            </div>
+            <div className="task-info__filters">
+              <ul className="filters">
+                <li
+                  className={`filters__filter ${
+                    filter == "All" ? "filters__filter--active" : ""
+                  }`}
+                  onClick={() => setFilter("All")}
+                >
+                  All
+                </li>
+                <li
+                  className={`filters__filter ${
+                    filter == "Active" ? "filters__filter--active" : ""
+                  }`}
+                  onClick={() => setFilter("Active")}
+                >
+                  Active
+                </li>
+                <li
+                  className={`filters__filter ${
+                    filter == "Completed" ? "filters__filter--active" : ""
+                  }`}
+                  onClick={() => setFilter("Completed")}
+                >
+                  Completed
+                </li>
+              </ul>
+            </div>
+            <div
+              className="task-info__clear-completed"
+              onClick={clearCompleted}
+            >
+              Clear Completed
+            </div>
+          </div>
+
+          <div className="reorder-note">Drag and drop to reorder list</div>
+        </div>
       </div>
     </div>
   );
 }
 
-function Task(props) {
-  const [checked, setChecked] = useState(false);
+function Task({
+  id,
+  name,
+  completed,
+  toggleTaskCompleted,
+  deleteTask,
+  editTask,
+  reorderTask,
+  dragTask,
+  ...restProps
+}) {
+  const [activeTopDrop, setActiveTopDrop] = useState(false);
+  const [activeBottomDrop, setActiveBottomDrop] = useState(false);
+
+  function onNameChange(e) {
+    editTask(id, e.target.value);
+  }
+
+  function onRemoveClick() {
+    deleteTask(id);
+  }
+
+  function onCheckboxClick() {
+    toggleTaskCompleted(id);
+  }
+
+  function onDragStart(e) {
+    dragTask(id);
+  }
+
+  function onDragEnterTop(e) {
+    e.preventDefault();
+    setActiveTopDrop(true);
+  }
+
+  function onDragLeaveTop(e) {
+    e.preventDefault();
+    setActiveTopDrop(false);
+  }
+
+  function onDragEnterBottom(e) {
+    e.preventDefault();
+    setActiveBottomDrop(true);
+  }
+
+  function onDragLeaveBottom(e) {
+    e.preventDefault();
+    setActiveBottomDrop(false);
+  }
+
+  function onDropAbove(e) {
+    e.preventDefault();
+    reorderTask("above", id);
+    setActiveBottomDrop(false);
+    setActiveTopDrop(false);
+  }
+
+  function onDropBelow(e) {
+    e.preventDefault();
+    reorderTask("below", id);
+    setActiveBottomDrop(false);
+    setActiveTopDrop(false);
+  }
 
   return (
-    <div className="Task">
+    <div
+      {...restProps}
+      className={`task task--entered card card--shadowed ${
+        activeTopDrop
+          ? "card--active-drop-top"
+          : activeBottomDrop
+          ? "card--active-drop-bottom"
+          : ""
+      }`}
+      onDragStart={onDragStart}
+    >
       <div
-        className={`Task__Checkbox ${checked ? "Task__Checkbox--Checked" : ""}`}
-        onClick={() => (checked ? setChecked(false) : setChecked(true))}
+        className="dropzone dropzone--top"
+        onDragEnter={onDragEnterTop}
+        onDragOver={(e) => e.preventDefault()}
+        onDragLeave={onDragLeaveTop}
+        onDrop={onDropAbove}
+      />
+      <div
+        className={`checkbox checkbox--clickable ${
+          completed ? "checkbox--checked" : ""
+        }`}
+        onClick={onCheckboxClick}
       >
-        {checked ? <CheckIcon /> : null}
+        {completed ? <CheckIcon /> : null}
       </div>
       <input
         type="text"
-        className={`Task__Input ${checked ? "Task__Input--Checked" : ""}`}
+        className={`task__input ${completed ? "task__input--checked" : ""}`}
+        value={name}
+        onChange={onNameChange}
       />
-      <CrossIcon className="Task__Remove" />
+      <CrossIcon className="task__remove" onClick={onRemoveClick} />
+      <div
+        className="dropzone dropzone--bottom"
+        onDragEnter={onDragEnterBottom}
+        onDragOver={(e) => e.preventDefault()}
+        onDragLeave={onDragLeaveBottom}
+        onDrop={onDropBelow}
+      />
+    </div>
+  );
+}
+
+function TaskInput({ tasks, setTasks }) {
+  function onKeyDown(e) {
+    if (e.key === "Enter") {
+      const newTask = {
+        id: uuidv4(),
+        name: e.target.value,
+      };
+      setTasks([...tasks, newTask]);
+    }
+  }
+
+  return (
+    <div className="task card">
+      <div className={`checkbox`}></div>
+      <input
+        type="text"
+        className={`task__input`}
+        onKeyDown={onKeyDown}
+        placeholder="Create a new todo"
+      />
     </div>
   );
 }
